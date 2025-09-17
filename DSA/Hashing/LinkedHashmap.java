e949e732a91863141c6c283d8972f0c8f338aa75
Ranjit Kadam <ranjitkadam0307@gmail.com>
2025-09-17 19:01:26 +0530

    Hashing

---
+++ DSA/Hashing/LinkedHashmap.java
@@ -0,0 +1,24 @@
+package DSA.Hashing;
+
+import java.util.*;
+public class LinkedHashmap {
+    public static void main(String args[]){
+        LinkedHashMap<String,Integer> lhm = new LinkedHashMap<>();
+
+        lhm.put("first",1);
+        lhm.put("second",2);
+        lhm.put("third",3);
+        lhm.put("one",1);
+
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        hm.put("first",1);
+        hm.put("second",2);
+        hm.put("third",3);
+        hm.put("one",1);
+
+        //here we can compare the difference in order
+        System.out.println(lhm);
+        System.out.println(hm);
+    }
+}
---
+++ DSA/Hashing/hashSet.java
@@ -0,0 +1,27 @@
+package DSA.Hashing;
+
+import java.util.*;
+
+public class hashSet {
+   public static void main(String args[]){
+    HashSet <Integer> hs = new HashSet<>();
+
+    hs.add(1);
+    hs.add(2);
+    hs.add(3);
+    hs.add(3);
+    hs.add(1);
+
+    System.out.println(hs);
+
+    System.out.println(hs.contains(1));
+
+    hs.remove(1);
+
+    System.out.println(hs);
+
+
+
+
+   } 
+}
